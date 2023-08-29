fc12b9350c0c3e38d41bf4b61f88897d5a209cb0
sanjeevnode <me.sanjeevks@gmail.com>
2023-08-29 22:57:16 +0530

    done

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from 'dotenv'
+import userRoutes from "./routes/userRoutes.js"
+dotenv.config();
+import { notFound ,errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+
+connectDB();
+
+const port = process.env.PORT||5000;
+
+const app = express()
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}));
+
+app.use(cookieParser());
+
+app.use('/api/users',userRoutes);
+
+app.get("/",(req,res)=>{
+    res.json({message:"server started"})
+})
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port,()=>console.log(`server started onn port ${port}` )) 
